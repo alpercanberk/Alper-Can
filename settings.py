d376f7fb6feedd03f60dca63f5e23b7087ab47cf
alpertucanberk <alper.tu.canberk@gmail.com>
2016-07-13 22:23:04 -0700

    Stuff

--- settings.py
+++ settings.py
@@ -109,17 +109,16 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_in_virtualenv","static_root")
+# STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_in_virtualenv","static_root")
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
-
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static_in_pro","our_static"),
-    #os.path.join(BASE_DIR, "static_in_virtualenv"),
-
-    #'/var/www/static/',
+    os.path.join(PROJECT_ROOT, 'static'),
 )
-
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(os.path.dirname(BASE_DIR), "static_in_virtualenv","media_root")
