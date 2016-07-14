ec19c953900e2e9092aae6912497d5f378976198
alpertucanberk <alper.tu.canberk@gmail.com>
2016-07-13 22:28:39 -0700

    Fixes

--- settings.py
+++ settings.py
@@ -117,10 +117,10 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(os.path.dirname(BASE_DIR), "static")
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 
