ba313df213d80f1e91322253edab96c4d53097ba
alpertucanberk <alper.tu.canberk@gmail.com>
2016-07-15 21:15:18 -0700

    statics

--- settings.py
+++ settings.py
@@ -114,6 +114,7 @@ STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, 'static_in_pro')
 )
 
 MEDIA_URL='/media/'
